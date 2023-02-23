d343f86ddd83d2260cac0c704ff5dc1c00d87192
Marius CA <marius@example.com>
2023-02-23 09:47:23 +0200

    shoppingList

---
+++ js/reduce.js
@@ -0,0 +1,16 @@
+'use strict';
+console.log('reduce.js file was loaded');
+
+const shoppingList = [
+  { item: 'Apples', description: 'A bag of fresh apples', quantity: 3 },
+  { item: 'Milk', description: 'A carton of whole milk', quantity: 1 },
+  { item: 'Bread', description: 'A loaf of sourdough bread', quantity: 2 },
+  { item: 'Eggs', description: 'A dozen large eggs', quantity: 2 },
+  { item: 'Cheese', description: 'A block of cheddar cheese', quantity: 1 },
+  { item: 'Yogurt', description: 'A container of Greek yogurt', quantity: 3 },
+  {
+    item: 'Chicken',
+    description: 'A pack of boneless chicken breasts',
+    quantity: 4,
+  },
+];
