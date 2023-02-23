cbda3b26a6c70191bdf06a3bc34694bc0e013251
Marius CA <marius@example.com>
2023-02-23 09:59:15 +0200

    // su forEach sudeti

--- js/reduce.js
+++ js/reduce.js
@@ -1,6 +1,11 @@
 'use strict';
 console.log('reduce.js file was loaded');
 
+// array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+// skirtas sumazinti masyvo kazkkokias reiksmes iki vienos reikmes ar objekto.
+// grazina viena reikmes kuri akumuliuoasi 'total'
+// initialValue - galima rasyti arba ne, rekomendacija iki tam tikro lygio visada rasyti.
+
 const shoppingList = [
   { item: 'Apples', description: 'A bag of fresh apples', quantity: 3 },
   { item: 'Milk', description: 'A carton of whole milk', quantity: 1 },
@@ -10,7 +15,11 @@ const shoppingList = [
   { item: 'Yogurt', description: 'A container of Greek yogurt', quantity: 3 },
   {
     item: 'Chicken',
-    description: 'A pack of boneless chicken breasts',
+    description: 'Boneless chicken breasts',
     quantity: 4,
   },
 ];
+
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+// su forEach sudeti visas numbers reikmes i viena ir atspaudinti suma
